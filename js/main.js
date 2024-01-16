1ab643a4be16dca31833afeaac73f4bab72c055c
Anderson <uhuander@gmail.com>
2024-01-16 18:00:19 -0300

    refactoring-code-part-2

--- js/main.js
+++ js/main.js
@@ -4,9 +4,16 @@ const engine = {
   modeChanged: false,
   intervalId: null, // Armazena o ID do intervalo do relógio
 
-  secondsPoint: document.querySelector(".seconds"),
-  minutesPoint: document.querySelector(".minutes"),
-  hourPoint: document.querySelector(".hours"),
+  secondHand: document.querySelector(".seconds"),
+  minuteHand: document.querySelector(".minutes"),
+  hourHand: document.querySelector(".hours"),
+
+  // cronografo
+  intervalIdChrono: null,
+  seconds: 0,
+  minutes: 0,
+
+  secPosition: 0,
 
   createMarcadores: function () {
     let marcadores = document.querySelector(".marcadores");
@@ -61,11 +68,132 @@ const engine = {
       let minPosition = date.getMinutes() * 6 + date.getSeconds() / 10;
       let hourPosition = date.getHours() * 30 + date.getMinutes() / 2;
 
-     
-      this.secondsPoint.style.transform = `rotate(${secPosition}deg)`;
-      this.minutesPoint.style.transform = `rotate(${minPosition - 180}deg)`;
-      this.hourPoint.style.transform = `rotate(${hourPosition - 180}deg)`;
+      this.secondHand.style.transform = `rotate(${secPosition}deg)`;
+      this.minuteHand.style.transform = `rotate(${minPosition - 180}deg)`;
+      this.hourHand.style.transform = `rotate(${hourPosition - 180}deg)`;
     }, 1000);
+  },
+
+  stopClock: function () {
+    clearInterval(this.intervalId);
+  },
+
+  resetSecondAndMinuteHands: function () {
+    // this.secondHand = document.querySelector(".seconds");
+    // this.minuteHand = document.querySelector(".minutes");
+
+    // Definindo uma transição suave para os ponteiros
+    this.secondHand.style.transition = "transform 1s ease-in-out";
+    this.minuteHand.style.transition = "transform 1s ease-in-out";
+
+    // Definindo a posição inicial dos ponteiros após um pequeno atraso
+    setTimeout(() => {
+      this.secondHand.style.transform = "rotate(180deg)";
+      this.minuteHand.style.transform = "rotate(180deg)";
+    }, 50); // Tempo de espera antes de aplicar a transformação
+  },
+
+  startChronograph: function () {
+    this.secondHand.style.transition = "none";
+    this.minuteHand.style.transition = "none";
+
+    this.intervalIdChrono = setInterval(() => {
+      this.seconds++;
+
+      let secPosition = this.seconds * 6 + 180;
+      this.secondHand.style.transform = `rotate(${secPosition}deg)`;
+
+      if (this.seconds === 60) {
+        this.seconds = 0;
+        this.minutes++;
+        let minPosition = this.minutes * 6 + this.seconds / 10 - 180;
+        this.minuteHand.style.transform = `rotate(${minPosition}deg)`;
+      }
+    }, 1000);
+  },
+
+  stopChronograph: function () {
+    clearInterval(this.intervalIdChrono);
+  },
+
+  resetChronograph: function () {
+    clearInterval(this.intervalIdChrono);
+
+    // Zera os segundos e minutos
+    this.seconds = 0;
+    this.minutes = 0;
+
+    this.secondHand.style.transition = "transform 1s ease-in-out";
+    this.minuteHand.style.transition = "transform 1s ease-in-out";
+
+    // Define a rotação dos ponteiros de segundos e minutos para 0 graus
+    this.secondHand.style.transform = "rotate(180deg)";
+    this.minuteHand.style.transform = "rotate(180deg)";
+
+    setTimeout(() => {
+      this.secondHand.style.transition = "none";
+      this.minuteHand.style.transition = "none";
+    }, 2000);
+  },
+
+  changetextInside: function () {},
+
+  changeMode: function () {
+    //start and stop buttons
+    const startButton = document.getElementById("start");
+    const stopButton = document.getElementById("stop");
+    const resetButton = document.getElementById("reset");
+
+    //text Inside
+    let textInside = document.querySelectorAll(".textInside");
+
+    textInside.forEach((text) => {
+      text.style.display = this.modeChanged ? "block" : "none";
+    });
+
+    // date
+    let date = document.querySelectorAll(".data");
+
+    date.forEach((text) => {
+      text.style.display = this.modeChanged ? "block" : "none";
+    });
+
+    // hour hand
+    let hours = document.querySelectorAll(".hours");
+
+    hours.forEach((text) => {
+      text.style.display = this.modeChanged ? "block" : "none";
+    });
+
+    //Verifica se o relógio está em execução para parar ou iniciar conforme necessário
+    if (this.modeChanged) {
+      this.startClock();
+      modeButton.innerHTML = "Modo cronógrafo";
+
+      startButton.style.display = "none";
+      stopButton.style.display = "none";
+      resetButton.style.display = "none";
+
+      //parar e zerar crono
+      this.stopChronograph();
+      this.resetChronograph();
+
+      // alert("agora");
+    } else {
+      this.stopClock();
+      this.resetSecondAndMinuteHands();
+
+      this.changetextInside();
+
+      modeButton.innerHTML = "Modo relógio";
+
+      startButton.style.display = "block";
+      stopButton.style.display = "block";
+      resetButton.style.display = "block";
+    }
+
+
+    this.modeChanged = !this.modeChanged;
   },
 
   createData: function () {
@@ -88,170 +216,4 @@ const engine = {
   },
 };
 
-console.log(engine);
-
-// cronografo
-let intervalIdChrono;
-let seconds = 0;
-let minutes = 0;
-
-function startChronograph() {
-  engine.secondsPoint.style.transition = 'none';
-  engine.minutesPoint.style.transition = 'none';
-  
-  intervalIdChrono = setInterval(() => {
-    // let secondsPoint = document.querySelector(".seconds");
-    // let minutesPoint = document.querySelector(".minutes");
-
-    seconds++;
-
-    let secPosition = seconds * 6 + 180;
-    engine.secondsPoint.style.transform = `rotate(${secPosition}deg)`;
-
-    if (seconds === 60) {
-      seconds = 0;
-      minutes++;
-      let minPosition = minutes * 6 + seconds / 10 - 180;
-      engine.minutesPoint.style.transform = `rotate(${minPosition}deg)`;
-    }
-  }, 1000);
-}
-
-function stopChronograph() {
-  clearInterval(intervalIdChrono);
-}
-
-function resetChronograph() {
-  clearInterval(intervalIdChrono);
-
-  // let secondsPoint = document.querySelector(".seconds");
-  // let minutesPoint = document.querySelector(".minutes");
-
-  // Zera os segundos e minutos
-  seconds = 0;
-  minutes = 0;
-
-  engine.secondsPoint.style.transition = "transform 1s ease-in-out";
-  engine.minutesPoint.style.transition = "transform 1s ease-in-out";
-
-  // Define a rotação dos ponteiros de segundos e minutos para 0 graus
-  engine.secondsPoint.style.transform = "rotate(180deg)";
-  engine.minutesPoint.style.transform = "rotate(180deg)";
-
-  setTimeout(()=>{
-    engine.secondsPoint.style.transition = 'none';
-    engine.minutesPoint.style.transition = "none";
-  },2000);
-}
-
-function stopClock() {
-  clearInterval(engine.intervalId);
-}
-
-// function resetSecondAndMinuteHands() {
-//   let secondsPoint = document.querySelector(".seconds");
-//   let minutesPoint = document.querySelector(".minutes");
-
-//   // Adicionando uma transição suave para os ponteiros
-//   secondsPoint.style.transition = "transform 1s ease-in-out";
-//   minutesPoint.style.transition = "transform 1s ease-in-out";
-
-//   // Define a posição dos ponteiros de segundo e minuto para 0 graus
-//   secondsPoint.style.transform = "rotate(180deg)";
-//   minutesPoint.style.transform = "rotate(180deg)";
-
-//   // Removendo a transição após o efeito
-//   setTimeout(() => {
-//     secondsPoint.style.transition = "";
-//     minutesPoint.style.transition = "";
-//   }, 1000); // Tempo total de 1 segundo para a transição
-// }
-
-function changetextInside() {}
-
-
-function resetSecondAndMinuteHands() {
-  let secondsPoint = document.querySelector(".seconds");
-  let minutesPoint = document.querySelector(".minutes");
-
-  // Definindo uma transição suave para os ponteiros
-  secondsPoint.style.transition = "transform 1s ease-in-out";
-  minutesPoint.style.transition = "transform 1s ease-in-out";
-
-  // Definindo a posição inicial dos ponteiros após um pequeno atraso
-  setTimeout(() => {
-    secondsPoint.style.transform = "rotate(180deg)";
-    minutesPoint.style.transform = "rotate(180deg)";
-  }, 50); // Tempo de espera antes de aplicar a transformação
-
-
-
-
-}
-
-
-
-function changeMode() {
-  //start and stop buttons
-  const startButton = document.getElementById("start");
-  const stopButton = document.getElementById("stop");
-  const resetButton = document.getElementById("reset");
-
-  //text Inside
-  let textInside = document.querySelectorAll(".textInside");
-
-  textInside.forEach((text) => {
-    text.style.display = engine.modeChanged ? "block" : "none";
-  });
-
-  // date
-  let date = document.querySelectorAll(".data");
-
-  date.forEach((text) => {
-    text.style.display = engine.modeChanged ? "block" : "none";
-  });
-
-  // hour hand
-  let hours = document.querySelectorAll(".hours");
-
-  hours.forEach((text) => {
-    text.style.display = engine.modeChanged ? "block" : "none";
-  });
-
-  //Verifica se o relógio está em execução para parar ou iniciar conforme necessário
-  if (engine.modeChanged) {
-    engine.startClock();
-    modeButton.innerHTML = "Modo cronógrafo";
-
-    startButton.style.display = "none";
-    stopButton.style.display = "none";
-    resetButton.style.display = "none";
-
-    //parar e zerar crono
-    stopChronograph();
-    resetChronograph();
-
-    // alert("agora");
-  } else {
-    stopClock();
-    resetSecondAndMinuteHands();
-
-    changetextInside();
-
-    modeButton.innerHTML = "Modo relógio";
-
-    startButton.style.display = "block";
-    stopButton.style.display = "block";
-    resetButton.style.display = "block";
-  }
-
-  //Reinicia os ponteiros de segundo e minuto se o modo for alterado
-  // if (!engine.modeChanged) {
-  //   resetSecondAndMinuteHands();
-  // }
-
-  engine.modeChanged = !engine.modeChanged;
-}
-
-/* date clock */
 engine.start();
